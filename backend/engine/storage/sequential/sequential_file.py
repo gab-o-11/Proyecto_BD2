8b41dfd5d285a4325cbbd49e6455e8bd89662078
gabo <gabriel.vargas@utec.edu.pe>
2026-09-17 18:58:36 -0500

    feat(sequential File): se agrega la función insert, se modifican las funciones auxiliares para utilizar la estrategia de overflow con punteros

--- backend/engine/storage/sequential/sequential_file.py
+++ backend/engine/storage/sequential/sequential_file.py
@@ -3,15 +3,16 @@ import struct
 
 
 class SequentialFile:
-    FILE_HEADER_FORMAT = "iiii" # total_registros, total_activos, total_eliminados, record_size
+    FILE_HEADER_FORMAT = "iiiii"  # total_registros, total_activos, total_eliminados, record_size, overflow_head
 
-    def __init__(self, filename, record_format, key_index=0):
+    def __init__(self, filename, record_format, key_index=0, overflow_limit=3):
         self.filename = filename
         self.RECORD_FORMAT = record_format
         self.key_index = key_index
-        self.RECORD_FLAG_FORMAT = self.RECORD_FORMAT + "b"
-        self.RECORD_FLAG_SIZE = struct.calcsize(self.RECORD_FLAG_FORMAT)
+        self.overflow_limit = overflow_limit
+        self.RECORD_WITH_POINTER_FORMAT = self.RECORD_FORMAT + "i"
         self.RECORD_SIZE = struct.calcsize(self.RECORD_FORMAT)
+        self.RECORD_WITH_POINTER_SIZE = struct.calcsize(self.RECORD_WITH_POINTER_FORMAT)
         self.FILE_HEADER_SIZE = struct.calcsize(self.FILE_HEADER_FORMAT)
 
         if not os.path.exists(filename) or os.path.getsize(filename) == 0:
@@ -19,19 +20,19 @@ class SequentialFile:
 
     def nuevo_archivo(self):
         with open(self.filename, "wb") as f:
-            f.write(struct.pack(self.FILE_HEADER_FORMAT, 0, 0, 0, self.RECORD_FLAG_SIZE))
+            f.write(struct.pack(self.FILE_HEADER_FORMAT, 0, 0, 0, self.RECORD_WITH_POINTER_SIZE, -1))
 
     def read_header(self):
         with open(self.filename, "rb") as f:
             data = f.read(self.FILE_HEADER_SIZE)
             if len(data) != self.FILE_HEADER_SIZE:
                 raise ValueError("Header del archivo corrupto o vacío")
-            total_records, total_activos, total_eliminados, record_size = struct.unpack(
+            total_records, total_activos, total_eliminados, record_size, overflow_head = struct.unpack(
                 self.FILE_HEADER_FORMAT, data
             )
-            return total_records, total_activos, total_eliminados, record_size
+            return total_records, total_activos, total_eliminados, record_size, overflow_head
 
-    def write_header(self, total_records, total_activos, total_eliminados, record_size):
+    def write_header(self, total_records, total_activos, total_eliminados, record_size, overflow_head):
         with open(self.filename, "r+b") as f:
             f.seek(0)
             f.write(struct.pack(
@@ -40,21 +41,42 @@ class SequentialFile:
                 total_activos,
                 total_eliminados,
                 record_size,
+                overflow_head,
             ))
 
     def read_record(self, position):
         with open(self.filename, "rb") as f:
-            f.seek(self.FILE_HEADER_SIZE + position * self.RECORD_FLAG_SIZE)
-            raw = f.read(self.RECORD_FLAG_SIZE)
-            if len(raw) != self.RECORD_FLAG_SIZE:
+            offset = self.FILE_HEADER_SIZE + position * self.RECORD_WITH_POINTER_SIZE
+            f.seek(offset)
+            raw = f.read(self.RECORD_WITH_POINTER_SIZE)
+            if len(raw) != self.RECORD_WITH_POINTER_SIZE:
                 return None
-            return struct.unpack(self.RECORD_FLAG_FORMAT, raw)
+            return struct.unpack(self.RECORD_WITH_POINTER_FORMAT, raw)
 
     def write_record(self, position, record_tuple):
         with open(self.filename, "r+b") as f:
-            offset = self.FILE_HEADER_SIZE + position * self.RECORD_FLAG_SIZE
+            offset = self.FILE_HEADER_SIZE + position * self.RECORD_WITH_POINTER_SIZE
             f.seek(offset)
-            f.write(struct.pack(self.RECORD_FLAG_FORMAT, *record_tuple))
+            f.write(struct.pack(self.RECORD_WITH_POINTER_FORMAT, *record_tuple))
 
     def get_key(self, record):
         return record[self.key_index]
+
+    def insert(self, record):
+        record = tuple(record)
+        total_records, total_activos, total_eliminados, _, overflow_head = self.read_header()
+        new_position = total_records
+        self.write_record(new_position, record + (overflow_head,))
+        overflow_head = new_position
+        total_records += 1
+        total_activos += 1
+
+        self.write_header(
+            total_records,
+            total_activos,
+            total_eliminados,
+            self.RECORD_WITH_POINTER_SIZE,
+            overflow_head,
+        )
+
+        return new_position
