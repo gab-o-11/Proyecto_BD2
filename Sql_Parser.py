0a43c787c09f0a08ae53d34c77fd2ed25a449965
flxamartinez <flxamartinez@utec.edu.pe>
2026-09-17 18:18:57 -0500

    feat(sql_parser): implmentacion de program, statemente, select ee insert

--- Sql_Parser.py
+++ Sql_Parser.py
@@ -48,21 +48,73 @@ class Parser:
         )
 
 
+    # program -> statement { ';' statement } [ ';' ] EOF
     def parse_program(self):
-        pass
+        sentencias = [self._statement()]
+        while self._match(TokenType.SEMICOLON):
+            if self._check(TokenType.EOF):
+                break
+            sentencias.append(self._statement())
+        self._expect(TokenType.EOF, "';' o fin de la consulta")
+        return sentencias
 
+    # statement -> select | insert | delete
     def _statement(self):
-        pass
+        if self._match(TokenType.SELECT):
+            return self._select()
+        if self._match(TokenType.INSERT):
+            return self._insert()
+        if self._match(TokenType.DELETE):
+            return self._delete()
+        raise ParseError(
+            f"Línea {self.current.line}: se esperaba SELECT, INSERT o DELETE, "
+            f"se encontró '{self.current.lexeme}'"
+        )
 
+    # select -> SELECT columns FROM IDENTIFIER [ where ] [ group ] [ order ]
     def _select(self):
-        pass
+        if self._match(TokenType.STAR):
+            columnas = None
+        else:
+            columnas = [self._expect(TokenType.IDENTIFIER, "'*' o nombre de columna").lexeme]
+            while self._match(TokenType.COMMA):
+                columnas.append(self._expect(TokenType.IDENTIFIER, "nombre de columna").lexeme)
 
+        self._expect(TokenType.FROM, "FROM")
+        tabla = self._expect(TokenType.IDENTIFIER, "nombre de tabla").lexeme
+
+        condicion = None
+        if self._match(TokenType.WHERE):
+            condicion = self._condition()
+
+        group_by = None
+        if self._match(TokenType.GROUP):
+            self._expect(TokenType.BY, "BY")
+            group_by = self._expect(TokenType.IDENTIFIER, "nombre de columna").lexeme
+
+        order_by = None
+        if self._match(TokenType.ORDER):
+            self._expect(TokenType.BY, "BY")
+            order_by = self._expect(TokenType.IDENTIFIER, "nombre de columna").lexeme
+
+        return ("select", tabla, columnas, condicion, group_by, order_by)
+
+
+    # insert -> INSERT INTO IDENTIFIER VALUES '(' value { ',' value } ')'
     def _insert(self):
-        pass
+        self._expect(TokenType.INTO, "INTO")
+        tabla = self._expect(TokenType.IDENTIFIER, "nombre de tabla").lexeme
+        self._expect(TokenType.VALUES, "VALUES")
+        self._expect(TokenType.LPAREN, "'('")
+        valores = [self._value()]
+        while self._match(TokenType.COMMA):
+            valores.append(self._value())
+        self._expect(TokenType.RPAREN, "')'")
+        return ("insert", tabla, valores)
 
     ## delete -> DELETE FROM IDENTIFIER WHERE condition
     def _delete(self):
-        self._expect(TokenType.DELETE, "FROM")
+        self._expect(TokenType.FROM, "FROM")
         tabla = self._expect(TokenType.IDENTIFIER, "nombre de la tabla").lexeme
         self._expect(TokenType.WHERE, "WHERE")
         condicion = self._condition()
@@ -73,7 +125,7 @@ class Parser:
     def _condition(self):
         columna = self._expect(TokenType.IDENTIFIER, "nombre de la tabla").lexeme
 
-        if self._match(COMPARISON_OPS):
+        if self._match(*COMPARISON_OPS):
             operador = self.previous.lexeme
             valor = self._value()
             return ("condition",columna,operador, valor)
@@ -86,7 +138,7 @@ class Parser:
 
     ## value -> INT | FLOAT | STRING
     def _value(self):
-        if self._match(VALUE_TYPES):
+        if self._match(*VALUE_TYPES):
             return self.previous.lexeme
         raise ParseError(
             f"Línea {self.current.line}: se esperaba un INT, FLOAT, STRING, "
