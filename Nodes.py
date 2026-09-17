b3135e22193827f3c700320e693f573cb75b1ffa
flxamartinez <flxamartinez@utec.edu.pe>
2026-09-17 18:30:03 -0500

    geat(nodes): implementacion del compare, select, insert y delete como objetos

--- Nodes.py
+++ Nodes.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+from typing import List, Optional
 
 class Node:
     def accept(self, visitor):
@@ -7,7 +7,28 @@ class Node:
         pass
 
 
-# TODO: nodos de condiciones
+@dataclass
+class Compare(Node):
+    column: str
+    op: str
+    value: str
 
 
-# TODO: nodos de sentencias (SELECT, INSERT, DELETE)
+@dataclass
+class Select(Node):
+    table: str
+    columns: Optional[List[str]]
+    where: Optional[Compare] = None
+    group_by: Optional[str] = None
+    order_by: Optional[str] = None
+
+@dataclass
+class Insert(Node):
+    table: str
+    values: List[str]
+
+
+@dataclass
+class Delete(Node):
+    table: str
+    where: Compare
