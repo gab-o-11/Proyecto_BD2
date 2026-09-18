bad50d6f2bcde77ce238ee163324da711e49926e
flxamartinez <flxamartinez@utec.edu.pe>
2026-09-17 19:36:31 -0500

    feat(nodes): se agrega para que soporte BEGING TRANSACTION Y END TRANSACTION

--- Nodes.py
+++ Nodes.py
@@ -3,16 +3,13 @@ from typing import List, Optional
 
 class Node:
     def accept(self, visitor):
-        # TODO
-        pass
-
+        return getattr(visitor, "visit_" + type(self).__name__)(self)
 
 @dataclass
 class Compare(Node):
     column: str
     op: str
     value: str
-
 
 @dataclass
 class Select(Node):
@@ -27,8 +24,15 @@ class Insert(Node):
     table: str
     values: List[str]
 
-
 @dataclass
 class Delete(Node):
     table: str
     where: Compare
+
+@dataclass
+class BeginTransaction(Node):
+    pass
+
+@dataclass
+class EndTransaction(Node):
+    pass
