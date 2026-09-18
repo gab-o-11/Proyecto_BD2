47a204f6603880dea70cb7362a5202de1998565f
javier1308 <javier.olivares@utec.edu.pe>
2026-09-17 20:03:21 -0500

    feat(concurrencia): agregar bloqueos exclusivos por recurso

--- backend/engine/transactions/__init__.py
+++ backend/engine/transactions/__init__.py
@@ -1,7 +1,13 @@
+from .lock_manager import LockError, LockManager, LockTimeoutError
 from .manager import TransactionManager
 from .models import Transaction, TransactionError, TransactionState
+from .resources import Resource
 
 __all__ = [
+    "LockError",
+    "LockManager",
+    "LockTimeoutError",
+    "Resource",
     "Transaction",
     "TransactionError",
     "TransactionManager",
