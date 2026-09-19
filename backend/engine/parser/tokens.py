10dd6c2fa48ff33b7544add0ed0ebbf46e366e8a
flxamartinez <felix.martinez@utec.edu.pe>
2026-09-18 19:14:45 -0500

    feat(token): se agrega el tipo date

--- backend/engine/parser/tokens.py
+++ backend/engine/parser/tokens.py
@@ -27,6 +27,7 @@ class TokenType(Enum):
     FLOAT_TYPE = auto()
     INT_TYPE = auto()
     VARCHAR_TYPE = auto()
+    DATE_TYPE = auto()
 
     ## ID y tipos de datos
 
@@ -49,6 +50,7 @@ class TokenType(Enum):
     LESS_EQUAL = auto()
     GREATER = auto()   ## >
     GREATER_EQUAL = auto()
+
 
     ## Fin
     EOF  = auto() ## se coloca para enlazarlo con el punto 2.1.4
@@ -73,6 +75,7 @@ KEYWORDS = {
     "FLOAT": TokenType.FLOAT_TYPE,
     "INT": TokenType.INT_TYPE,
     "VARCHAR": TokenType.VARCHAR_TYPE,
+    "DATE": TokenType.DATE_TYPE,
     "UPDATE": TokenType.UPDATE,
 
 }
